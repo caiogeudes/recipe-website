281e665643f81124ff7d4a091b2ee6125293205a
Caio <caiogeudes@gmail.com>
2024-07-29 22:13:32 -0300

    header adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <header className="bg-amber-100 shadow-md w-full flex items-center justify-between  size-32">
+        <img src="/logo.png" className="pl-10 size-24 w-64 "></img>
+        <img src="/user.png" className="pr-5 flex-shrink-0 size-10 w-16"></img>
+      </header>
     </main>
   );
 }
