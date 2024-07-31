4b9a34e298b4a5c96cccf1595c74e5af674eb088
Caio <caiogeudes@gmail.com>
2024-07-30 22:26:51 -0300

    alt das imagens adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
 
       {/* Cabeçalho */}
       <header className="bg-amber-100 shadow-md w-full flex items-center justify-between  size-32">
-        <img src="/logo.png" className="pl-10 size-24 w-64 "></img>
-        <img src="/user.png" className="pr-5 flex-shrink-0 size-10 w-16"></img>
+        <img src="/logo.png" className="pl-10 size-24 w-64" alt="logo"></img>
+        <img src="/user.png" className="pr-5 flex-shrink-0 size-10 w-16" alt="perfil"></img>
       </header>
 
       {/* Título */}
